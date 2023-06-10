c5c18d8f4e7b95ad821954078323b94ae84697a7
Preston Monroe <69488647+RedneckLovinLife@users.noreply.github.com>
2023-06-10 19:36:58 -0400

    Update Config.lua

--- pbrp_clothingitems/Config.lua
+++ pbrp_clothingitems/Config.lua
@@ -21,4 +21,26 @@ Config = {
 		itemName = 'qb_chain2', -- Item name in your database
 		label = 'QB Bling', -- Only used for notification.
 	},
+-- 	[3] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+-- 		set = {
+-- 			Mask = {
+-- 				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+-- 				c2 = 154, -- Which # within the category
+-- 				c3 = 0, -- Which color variation of c2
+-- 			},
+-- 		},
+-- 		itemName = 'qb_mask', -- Item name in your database
+-- 		label = 'QB Mask', -- Only used for notification.
+-- 	},
+-- 	[4] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+-- 		set = {
+-- 			Shoes = {
+-- 				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+-- 				c2 = 154, -- Which # within the category
+-- 				c3 = 0, -- Which color variation of c2
+-- 			},
+-- 		},
+-- 		itemName = 'qb_shoes', -- Item name in your database
+-- 		label = 'QB Shoes', -- Only used for notification.
+-- 	},
 }
