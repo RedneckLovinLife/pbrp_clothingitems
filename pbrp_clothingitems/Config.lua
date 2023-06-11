697b570d639e453007ca52246995f13a779d23c1
Preston Monroe <69488647+RedneckLovinLife@users.noreply.github.com>
2023-06-11 10:19:20 -0400

    Update Config.lua

--- pbrp_clothingitems/Config.lua
+++ pbrp_clothingitems/Config.lua
@@ -1,46 +1,66 @@
 Config = {
-	[1] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+	[1] = {
 		set = {
-			Accessory = {
-				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
-				c2 = 155, -- Which # within the category
-				c3 = 0, -- Which color variation of c2
+			face = {
+				c1 = 0, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
 			},
+			mask = {
+				c1 = 1, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			torso = {
+				c1 = 3, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			leg = {
+				c1 = 4, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			shoes = {
+				c1 = 6, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+   -- Add more pieces to the outfit here! copy paste here.
 		},
-		itemName = 'qb_chain1', -- Item name in your database
-		label = 'QB Bling', -- Only used for notification.
+		itemName = 'outfit1', -- Item name in your database
+		label = 'Fancy Outfit', -- Only used for notification.
 	},
+
 	[2] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
 		set = {
-			Accessory = {
-				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
-				c2 = 154, -- Which # within the category
-				c3 = 0, -- Which color variation of c2
+			face = {
+				c1 = 0, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
 			},
+			mask = {
+				c1 = 1, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			torso = {
+				c1 = 3, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			leg = {
+				c1 = 4, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+			shoes = {
+				c1 = 6, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 2, -- Which # within the category
+				c3 = 3, -- Which color variation of c2
+			},
+   -- Add more pieces to the outfit here! copy past here.
 		},
-		itemName = 'qb_chain2', -- Item name in your database
-		label = 'QB Bling', -- Only used for notification.
+		itemName = 'outfit2', -- Item name in your database
+		label = 'Fancy Outfit #2', -- Only used for notification.
 	},
--- 	[3] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
--- 		set = {
--- 			Mask = {
--- 				c1 = 1, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
--- 				c2 = 1, -- Which # within the category
--- 				c3 = 0, -- Which color variation of c2
--- 			},
--- 		},
--- 		itemName = 'qb_mask', -- Item name in your database
--- 		label = 'QB Mask', -- Only used for notification.
--- 	},
--- 	[4] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
--- 		set = {
--- 			Shoes = {
--- 				c1 = 6, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
--- 				c2 = 1, -- Which # within the category
--- 				c3 = 0, -- Which color variation of c2
--- 			},
--- 		},
--- 		itemName = 'qb_shoes', -- Item name in your database
--- 		label = 'QB Shoes', -- Only used for notification.
--- 	},
-}
