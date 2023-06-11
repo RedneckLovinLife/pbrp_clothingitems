9855b9d2ec5416651aa9cb8dd6c437cde9833983
Preston Monroe <69488647+RedneckLovinLife@users.noreply.github.com>
2023-06-11 10:20:46 -0400

    Update Config.lua

--- pbrp_clothingitems/Config.lua
+++ pbrp_clothingitems/Config.lua
@@ -1,5 +1,27 @@
 Config = {
-	[1] = {
+	[1] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+		set = {
+			Accessory = {
+				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 155, -- Which # within the category
+				c3 = 0, -- Which color variation of c2
+			},
+		},
+		itemName = 'qb_chain1', -- Item name in your database
+		label = 'QB Bling', -- Only used for notification.
+	},
+	[2] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+		set = {
+			Accessory = {
+				c1 = 7, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
+				c2 = 154, -- Which # within the category
+				c3 = 0, -- Which color variation of c2
+			},
+		},
+		itemName = 'qb_chain2', -- Item name in your database
+		label = 'QB Bling', -- Only used for notification.
+	},
+	[3] = {
 		set = {
 			face = {
 				c1 = 0, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
@@ -32,7 +54,7 @@ Config = {
 		label = 'Fancy Outfit', -- Only used for notification.
 	},
 
-	[2] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
+	[4] = { -- Each time you make a new outfit, copy/paste this entire block and make sure the number goes up by one.
 		set = {
 			face = {
 				c1 = 0, -- Component ID 0: Face\ 1: Mask\ 2: Hair\ 3: Torso\ 4: Leg\ 5: Parachute / bag\ 6: Shoes\ 7: Accessory\ 8: Undershirt\ 9: Kevlar\ 10: Badge\ 11: Torso 2
